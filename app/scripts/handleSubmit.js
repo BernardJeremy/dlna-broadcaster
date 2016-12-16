919f67842fb0ef8b3f37e5aadc154130ee36ec5f
BernardJeremy <jeremy@bernard.sh>
2016-12-16 22:39:41 +0100

    add upnp server feature

--- app/scripts/handleSubmit.js
+++ app/scripts/handleSubmit.js
@@ -1,16 +1,23 @@
 var electron = require('electron');
 var startServer = electron.remote.require('./server').startServer;
+var started = false;
 
 $(document).ready(function() {
   $( ".file-uploader" ).submit(function( event ) {
       event.preventDefault();
       
       var listItems = $(".file-list li");
+      var directoriesArray = []
       listItems.each(function(idx, currentLI) {
         var dir = $(currentLI).find('.file-list__name').html();
-        console.log(dir, ':', $(currentLI).data('path'));
+        directoriesArray.push({
+            name: dir,
+            path: $(currentLI).data('path'),
+        })
       });
 
-      startServer();
+      startServer(directoriesArray, function(err, msg) {
+          $('.file-uploader__submit-button').val(msg);
+      });
   });
 });
--- src/server.js
+++ src/server.js
@@ -1,3 +1,47 @@
-export function startServer (){
-    console.log("test");
+import Server from "upnpserver";
+
+let serverObj = null;
+
+export function startServer (directoriesArray, callback){
+    let msg = "";
+    if (serverObj !== null) {
+        stopServer().then((msg) => {
+            callback(null, msg);   
+        }).catch((msg) => {
+            callback(null, msg);               
+        });
+        return ;
+    }
+    directoriesArray = directoriesArray.filter((elem) => {
+        return {
+            path: elem.path,
+            mountPoint: '/' + elem.name,
+        }
+    });
+    console.log(directoriesArray);
+    serverObj = new Server({ "name": "DLNA-Broadcaster" }, directoriesArray);
+
+    serverObj.start();
+
+    msg = "Server successfully started"; 
+    callback(null, msg);   
+};
+
+export function stopServer (){
+    return new Promise((fullfill, reject) => {
+        if (serverObj === null) {
+            fullfill("Server not started");
+            return;
+        }
+
+        serverObj.stop((err, stopped) => {
+            if (stopped) {
+                serverObj = null;
+                fullfill("Server successfully stopped");
+            } else {
+                reject("Unable to stop the server")
+            }
+        });
+
+    });    
 };
